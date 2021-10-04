113bf32fb1a600e1b2f689e09da7a0993d67c755
Abiria <abiria.dev@gmail.com>
2021-10-04 18:05:48 +0900

    save sh4

--- setupNode.sh
+++ setupNode.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 # apt install make -y
+rm -r /root/n
 curl -L https://git.io/n-install | bash
 . ~/.bashrc
 /root/n/bin/n latest
-node -v
-npm -v
+echo node --version: $(node -v)
+echo npm --version: $(npm -v)
