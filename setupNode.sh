0cbe977d3022492e108115e8bdb6d96ad6adb2e3
Abiria <abiria.dev@gmail.com>
2021-10-04 18:14:16 +0900

    save sh7

--- setupNode.sh
+++ setupNode.sh
@@ -4,7 +4,9 @@
 rm /usr/local/bin/node
 rm /usr/local/bin/npm
 rm -r /root/n
-curl -L https://git.io/n-install | bash -y
+curl -L https://git.io/n-install  > n.sh
+./n.sh -y
+rm n.sh
 . ~/.bashrc
 /root/n/bin/n lts
 echo node --version: $(node -v)
