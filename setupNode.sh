3f9df639a2ab810ecb1b7e4ef5f539a5478f92f1
Abiria <abiria.dev@gmail.com>
2021-10-04 18:11:01 +0900

    save sh6

--- setupNode.sh
+++ setupNode.sh
@@ -4,8 +4,9 @@
 rm /usr/local/bin/node
 rm /usr/local/bin/npm
 rm -r /root/n
-curl -L https://git.io/n-install | bash
+curl -L https://git.io/n-install | bash -y
 . ~/.bashrc
-/root/n/bin/n latest
+/root/n/bin/n lts
 echo node --version: $(node -v)
 echo npm --version: $(npm -v)
+npx -y yarn install
